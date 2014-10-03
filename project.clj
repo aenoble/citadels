d7e162612d92c80994e1913f1196354c853f11c4
Andrew Noble <aenoble@gmail.com>
2014-10-03 09:25:53 -0400

    Adding deps files.

--- project.clj
+++ project.clj
@@ -3,4 +3,25 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2311"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                 [hiccups "0.3.0"]
+                 [com.cemerick/piggieback "0.1.3"]
+                 [weasel "0.4.0-SNAPSHOT"]]
+
+  :plugins [[lein-cljsbuild "1.0.3"]]
+
+  :source-paths ["src"]
+
+  :cljsbuilds {
+    :builds {
+      :server {
+        :source-paths["src/citadels/server"]
+        :compiler {
+          :language-in :ecmascript5
+          :language-out :ecmascript5
+          :target :nodejs
+          :output-to "server.js"
+          :optimizations :simple}}}}
+ )
--- src/citadels/core.clj
+++
@@ -1,6 +0,0 @@
-(ns citadels.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
